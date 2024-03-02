1a14e117e90df2875247d46d35da77adb4cb1498
Randy <randi.berkelaar@outlook.com>
2024-03-02 01:12:56 +0100

    bakker prijs

--- yo.js
+++ yo.js
@@ -1,6 +1,8 @@
 let aantalkoekjes = 0;
 let koekjesPerKlik = 1;
 let koekjesVeld = document.querySelector('h1');
+let bakkerPrijs = 20;
+// let bakkersVeld = document.querySelector('h2');
 
 function verhoogKoekjes(){
 
@@ -11,9 +13,11 @@ function verhoogKoekjes(){
 
 function bakker(){
 
-    if(aantalkoekjes >= 100) {
+    if(aantalkoekjes >= bakkerPrijs) {
         koekjesPerKlik += 1;
-        aantalkoekjes -= 100;
+        aantalkoekjes -= bakkerPrijs;
+        bakkerPrijs = Math.round(bakkerPrijs *1.7);
+        document.getElementById("test").innerHTML = "Bakker prijs: " + bakkerPrijs;
     } else {
         alert("te weinng koekjes om te kopen")
     }
