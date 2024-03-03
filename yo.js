aa3cb6e2daedb209a579be42f2851304be0ef9b4
Randy <randi.berkelaar@outlook.com>
2024-03-03 13:29:37 +0100

    veel css

--- yo.js
+++ yo.js
@@ -1,23 +1,19 @@
 let aantalkoekjes = 0;
 let koekjesPerKlik = 1;
-let koekjesVeld = document.querySelector('h1');
+let koekjesVeld = document.querySelector('h2');
 let bakkerPrijs = 20;
-// let bakkersVeld = document.querySelector('h2');
 
 function verhoogKoekjes(){
-
     aantalkoekjes += koekjesPerKlik;
     updateKoekjes();
-    
 }
 
 function bakker(){
-
     if(aantalkoekjes >= bakkerPrijs) {
         koekjesPerKlik += 1;
         aantalkoekjes -= bakkerPrijs;
         bakkerPrijs = Math.round(bakkerPrijs *1.7);
-        document.getElementById("test").innerHTML = "Bakker prijs: " + bakkerPrijs;
+        document.getElementById("test").innerHTML = "Power klik prijs: " + bakkerPrijs;
     } else {
         alert("te weinng koekjes om te kopen")
     }
@@ -32,3 +28,24 @@ function updateKoekjes() {
 
 koekje.addEventListener('click', verhoogKoekjes);
 klikUpgrade.addEventListener('click', bakker);
+
+
+
+img = document.getElementById("koekje");
+console.log(img)
+
+function enlargeImg() {
+
+    img.style.transform = "scale(1.5)";
+
+}
+
+enlargeImg()
+console.log(enlargeImg)
+
+
+koekje.addEventListener('click', enlargeImg);
+
+
+
+        
